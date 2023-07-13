97a3b0f53be8314e5bd10adb7f79aa7514d58f4c
YujinJeon888888 <117812553+YujinJeon888888@users.noreply.github.com>
2023-07-13 18:07:43 +0900

    FIx : 떨어질 때 더블점프 문제 해결

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -33,7 +33,7 @@ public class Player : MonoBehaviour
     void Update()//단발적 입력: 업데이트함수
     {
         //점프
-        if ((Input.GetButtonDown("Jump")&&!anim.GetBool("isJump")))
+        if ((Input.GetButtonDown("Jump")&&!anim.GetBool("isJump"))&&!(rigid.velocity.y < 0))
         {
             rigid.velocity = new Vector2(rigid.velocity.x, jump_power);
             anim.SetBool("isJump", true);
@@ -90,9 +90,8 @@ public class Player : MonoBehaviour
             if (isJumpButton)
             {
                 //점프
-                if (!anim.GetBool("isJump"))
+                if (!anim.GetBool("isJump") && !(rigid.velocity.y < 0))
                 {
-
                     rigid.velocity = new Vector2(rigid.velocity.x, jump_power);
                     anim.SetBool("isJump", true);
                 }
@@ -141,7 +140,7 @@ public class Player : MonoBehaviour
     {
         //레이어변경
         this.gameObject.layer = 7;
-       
+        
         //투명하게 바꾸기
         sprite_renderer.color = new Color(1, 1, 1, 0.4f);
 
