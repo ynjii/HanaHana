607b391a827990dac08562c5d198566b7d15e9e2
hyunmin0809 <khm010809@naver.com>
2023-07-16 01:24:57 +0900

    Add: Floating Arrow & Fake Iteam

--- Assets/Scripts/Obstacle/ObstacleController.cs
+++ Assets/Scripts/Obstacle/ObstacleController.cs
@@ -13,6 +13,8 @@ public class ObstacleController : MonoBehaviour
     {
         Move, //단방향으로 움직임
         MoveSide, //왔다갔다
+        Rotate, //돌면서 떨어짐
+        Floating, //둥둥 떠있는 모션
         ChangeStatus, //레이어랑 tag 바꿈
         ChangeColor //색깔 바꿈
     }
@@ -46,7 +48,12 @@ public class ObstacleController : MonoBehaviour
     private bool isMoving = false; //isTrigger 처리된 collider랑 부딪히면 true;
     private Vector3 initialPosition; //움직인 거리를 재기 위해 사용
     private float movedDistance = 0f; 
-    
+
+    private float floatingSpeed = 3f; //둥둥 떠다니는 속도
+    private float floatingHeight = 0.1f; // 둥둥 떠다니는 높이
+
+    private float rotateSpeed = 10000f;
+
     private Rigidbody2D rigid;
     private Tilemap tilemap;
 
@@ -61,10 +68,14 @@ public class ObstacleController : MonoBehaviour
             isMoving = true;   
         }
     }
-
+    
     private void Awake(){
         initialPosition = transform.position;
         tilemap = GetComponent<Tilemap>();
+        //트리거 없어도 계쏙 움직여야하는 애들은 시작할때 true 처리
+        if(obType == ObType.Floating){
+            isMoving = true;
+        }
     }
 
     private void Update() 
@@ -85,13 +96,18 @@ public class ObstacleController : MonoBehaviour
             case ObType.MoveSide:
                 
                 break;
+            case ObType.Rotate:
+                Rotate();
+                break;
+            case ObType.Floating:
+                StartCoroutine(FloatingCoroutine());
+                break;
             case ObType.ChangeStatus:
                 ChangeStatus(tagName, layerIndex);
-            break;
+                break;
             case ObType.ChangeColor:
-                Debug.Log("됨");
-                tilemap.color = new Color(1, 1, 1, 0.8f); //투명하게 바꾸기. 추후 수정.
-            break;
+                tilemap.color = new Color(1, 1, 1, 0.8f); //투명하게 바꾸기. 추후 색깔바꿀일 있으면 수정.
+                break;
         }
     }
     /// <summary>
@@ -128,8 +144,29 @@ public class ObstacleController : MonoBehaviour
     /// <summary>
     /// 좌우로 왔다갔다 움직임
     /// </summary>
-    private void MoveSide(){
-        
+    private void MoveSide()
+    {
+    }
+
+    /// <summary>
+    /// 회전
+    /// </summary>
+    private void Rotate()
+    {
+        transform.Rotate(0, 0, -Time.deltaTime * rotateSpeed, Space.Self);     
+    }
+
+    /// <summary>
+    /// 위아래로 움직이는
+    /// </summary>
+    private IEnumerator FloatingCoroutine()
+    {
+        while(true)
+        {
+            float newY = initialPosition.y + Mathf.Sin(Time.time * floatingSpeed) * floatingHeight;
+            transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+            yield return null;
+        }
     }
 
     //tag 와 layer을 변경
