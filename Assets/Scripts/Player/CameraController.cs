dca7fb44090823d40c14302c9c66b3f5e1181c28
ruruisryu <79790536+ruruisryu@users.noreply.github.com>
2023-07-16 13:18:52 +0900

    fix: 카메라 - 플레이어가 damaged상태가 되면 더이상 추적하지 않음

--- Assets/Scripts/Player/CameraController.cs
+++ Assets/Scripts/Player/CameraController.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.PlayerLoop;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] private Define.CameraState cameraState;
+    public Define.CameraState cameraState;
     [SerializeField] private Transform player;
+    private Player player_script;
     [SerializeField] private Vector3 delta; 
     void Awake()
     {
@@ -17,15 +19,23 @@ public class CameraController : MonoBehaviour
     {
         cameraState = Define.CameraState.Player;
         player = GameObject.FindWithTag("Player").transform;
-        delta = new Vector3(0f, 3f, -1f);
+        player_script = player.GetComponent<Player>();
+        delta = new Vector3(0f, 1f, -1f);
     }
 
     void LateUpdate()
     {
         // 플레이어가 피격당했다면 더 이상 따라가지 않음
-        // if (player) return;
-        
-        // 카메라의 position은 플레이어의 position에 델타값을 더한 값
-        transform.position = player.transform.position + delta;
+        if (player_script.player_state == Define.PlayerState.Damaged)
+        {
+            player = null;
+        }
+
+        if (!player.IsUnityNull())
+        {
+            // 카메라의 position은 플레이어의 position에 델타값을 더한 값
+            transform.position = player.transform.position + delta;
+            
+        }
     }
 }
