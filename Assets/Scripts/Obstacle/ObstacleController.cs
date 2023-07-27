5616240ce48c286278a491c4e314434c58a78819
ruruisryu <79790536+ruruisryu@users.noreply.github.com>
2023-07-27 23:59:19 +0900

    Revert "Main2 끌어오기"
    This reverts commit 71b5acd557cf881ec6dd908afcdd5bc3f131becc.

--- Assets/Scripts/Obstacle/ObstacleController.cs
+++ Assets/Scripts/Obstacle/ObstacleController.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
-using static Define;
 
 /// <summary>
 /// 움직이는 obstacle들의 부모 스크립트입니다.
@@ -110,9 +109,6 @@ public class ObstacleController : MonoBehaviour
     [SerializeField]
     private int layerIndex = 0;
 
-    [SerializeField]
-    private Define.Tags colTag = Define.Tags.Player; //player가 default
-
     private Vector3 initialPosition; //움직인 거리를 재기 위해 사용
     private Vector3 movement = Vector3.zero;
 
@@ -137,7 +133,7 @@ public class ObstacleController : MonoBehaviour
     /// 만약 isTriggred 처리된 collider를 사용하고 싶다면 주의해서 사용해주셔야해요.
     /// </summary>
     private void OnTriggerEnter2D(Collider2D collision) {
-        if (!isCol && collision.gameObject.CompareTag(colTag.ToString()))
+        if (!isCol && collision.gameObject.CompareTag("Player"))
         {
             StartCoroutine(SetIsmoving(true));
         }
@@ -148,7 +144,7 @@ public class ObstacleController : MonoBehaviour
     /// </summary>
     /// <param name="collision"></param>
     private void OnCollisionEnter2D(Collision2D collision) {
-        if (collision.transform.CompareTag(colTag.ToString()))
+        if (collision.gameObject.CompareTag("Player"))
         {
             if(isCol)
                 StartCoroutine(SetIsmoving(true));
@@ -160,7 +156,7 @@ public class ObstacleController : MonoBehaviour
     }
 
     private void OnCollisionExit2D(Collision2D collision) {
-        if(isPlatform && collision.transform.CompareTag(colTag.ToString())){
+        if(isPlatform && collision.transform.CompareTag("Player")){
             collision.transform.SetParent(null);
         }
     }
