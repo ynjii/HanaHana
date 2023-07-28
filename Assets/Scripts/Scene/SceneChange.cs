2acd4ba8e9ebb80f117ff25a52301425bc6686e1
dubidubob <2076397@ewhain.net>
2023-07-28 12:00:44 +0900

    에러 해결

--- Assets/Scripts/Scene/SceneChange.cs
+++ Assets/Scripts/Scene/SceneChange.cs
@@ -14,8 +14,8 @@ public class SceneChange : MonoBehaviour
 
     public void new_Change()
     {
-        if(EditorUtility.DisplayDialog("게임 세이브 정보 삭제", "정말 삭제 하시겠습니까?", "네", "아니오"))
-            PlayerPrefs.DeleteAll();
+        /*if(EditorUtility.DisplayDialog("게임 세이브 정보 삭제", "정말 삭제 하시겠습니까?", "네", "아니오"))*/
+        PlayerPrefs.DeleteAll();
         
         SceneManager.LoadScene("SnowWhite");
     }
