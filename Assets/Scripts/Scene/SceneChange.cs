9130e94e9392c2d98198df23d5d7f08dc4b8cba4
dubidubob <2076397@ewhain.net>
2023-07-28 11:25:09 +0900

    씬 연결

---
+++ Assets/Scripts/Scene/SceneChange.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneChange : MonoBehaviour
+{
+    public void Change()
+    {
+        SceneManager.LoadScene("SnowWhite");
+    }
+
+    public void new_Change()
+    {
+        if(EditorUtility.DisplayDialog("게임 세이브 정보 삭제", "정말 삭제 하시겠습니까?", "네", "아니오"))
+            PlayerPrefs.DeleteAll();
+        
+        SceneManager.LoadScene("SnowWhite");
+    }
+}
