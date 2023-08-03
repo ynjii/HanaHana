1322133be02bb1b1d828b2860a69442fdf314dc6
YujinJeon888888 <117812553+YujinJeon888888@users.noreply.github.com>
2023-08-03 22:31:24 +0900

    Add: Laser 하는 중

---
+++ Assets/Laser.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    [SerializeField] private float defDistanceRay = 100;
+    public Transform laserFirePoint;
+    public LineRenderer m_lineRenderer;
+    Transform m_transform;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        m_transform = GetComponent<Transform>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ShootLaser();
+    }
+
+    void ShootLaser()
+    {
+        if (Physics2D.Raycast(m_transform.position, transform.right))
+        {
+            RaycastHit2D _hit = Physics2D.Raycast(laserFirePoint.position, transform.right);
+            Draw2DRay(laserFirePoint.position, _hit.point); 
+        }
+        else
+        {
+            Draw2DRay(laserFirePoint.position, laserFirePoint.transform.right * defDistanceRay);
+        }
+    }
+
+    void Draw2DRay(Vector2 startPos, Vector2 endPos)
+    {
+        m_lineRenderer.SetPosition(0,startPos);
+        m_lineRenderer.SetPosition(1, endPos);
+
+    }
+}
