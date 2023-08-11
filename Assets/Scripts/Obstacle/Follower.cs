f99d747bf5c65b92c0750a57021dd88a8f5f4720
hyunmin0809 <khm010809@naver.com>
2023-08-11 22:13:11 +0900

    Fix: Follower

--- Assets/Scripts/Obstacle/Follower.cs
+++ Assets/Scripts/Obstacle/Follower.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static Define;
 
 public class Follower : MonoBehaviour
 {
@@ -8,23 +9,28 @@ public class Follower : MonoBehaviour
 
     [SerializeField]
     private int followDelay;
-    
+
     public Transform player;
     private Queue<Vector3> playerPos;
     private bool isTriggered = false;
     private Player playerScript;
     // Start is called before the first frame update
-    void OnCollisionEnter2D(Collision2D collision) {
-        if(collision.gameObject.CompareTag("Player"))
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
         {
-            isTriggered = true;   
+            isTriggered = true;
             this.gameObject.layer = 3;
             playerScript = collision.gameObject.GetComponent<Player>();
         }
-        else if(collision.gameObject.CompareTag("Enemy"))
+        if (isTriggered && collision.gameObject.CompareTag("Enemy"))
         {
+            Debug.Log("부딪힘");
             playerScript.onDamaged(collision.transform.position);
+            //게임 매니저의 게임오버 처리 실행
+            GameManager.instance.OnPlayerDead();
         }
+        Debug.Log(collision);
     }
 
     void Awake()
@@ -34,17 +40,18 @@ public class Follower : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(isTriggered){
+        if (isTriggered)
+        {
             Watch();
             Follow();
-        }   
+        }
     }
-    
+
     void Watch()
     {
-        if(!playerPos.Contains(player.position))
+        if (!playerPos.Contains(player.position))
             playerPos.Enqueue(player.position);
-        if(playerPos.Count > followDelay)
+        if (playerPos.Count > followDelay)
             followPos = playerPos.Dequeue();
     }
 
