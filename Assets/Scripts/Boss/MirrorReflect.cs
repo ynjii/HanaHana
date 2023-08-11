8cd5f1eb59448661473f8a2cde5173be83f069e7
ruruisryu <79790536+ruruisryu@users.noreply.github.com>
2023-08-11 18:20:19 +0900

    Add: 새로운 레이저 구현

--- Assets/Scripts/Boss/MirrorReflect.cs
+++ Assets/Scripts/Boss/MirrorReflect.cs
@@ -1,25 +1,27 @@
-﻿using UnityEngine;
+﻿using Unity.VisualScripting;
+using UnityEngine;
 using static Define;
 
 public class MirrorReflect : MonoBehaviour
 {
     private LineRenderer predictionLine;
     private RaycastHit2D predictionHit;
+    private LayerMask predictionLayerMask;
     private GameObject player;
     private Player player_script;
     private SpriteRenderer player_spriterenderer;
-    private bool is_first_entered;
 
+    [SerializeField] private bool isReflectable;
+    [SerializeField] private bool is_first_entered;
     [SerializeField] private Vector3 _direction;
-    [SerializeField] private LayerMask predictionLayerMask;
 
     private void Start()
     {
         predictionLine = GetComponent<LineRenderer>();
-        _direction = _direction.normalized;
         player = GameObject.FindWithTag("Player");
         player_script = player.GetComponent<Player>();
         player_spriterenderer = player.GetComponent<SpriteRenderer>();
+        predictionLayerMask = ~(1 << 8);
     }
 
     private void LateUpdate()
@@ -41,23 +43,44 @@ public class MirrorReflect : MonoBehaviour
         }*/
     }
 
-    private void DrawPredictionLine(Vector3 direction)
+    private void DrawPredictionLine(Vector3 _direction)
         {
             // Draw Prediction Line
             predictionLine.SetPosition(0, this.transform.position);
-            Debug.DrawRay(transform.position, direction * 10, Color.red);
-            predictionHit = Physics2D.Raycast(transform.position, direction, Mathf.Infinity, predictionLayerMask);
-    
-            if(predictionHit.collider == null)
+            Debug.DrawRay(transform.position, _direction * 10, Color.red);
+            predictionHit = Physics2D.Raycast(transform.position, _direction, Mathf.Infinity, predictionLayerMask);
+
+            if (predictionHit.collider.IsUnityNull())
             {
-                // no collision => don't draw prediction line
-                predictionLine.enabled = false;
+                predictionLine.SetPosition(1, new Vector3(_direction.x + transform.position.x, _direction.y + transform.position.y, 0));
                 return;
             }
-
+            
+            // 플레이어면 죽음
+            if (predictionHit.collider.CompareTag("Player"))
+            {
+                Vector2 target_pos = new Vector2(0, 0);
+                if (player_spriterenderer.flipX)
+                {
+                    target_pos = new Vector2(player.transform.position.x - 1, 0);
+                }
+                else
+                {
+                    target_pos = new Vector2(player.transform.position.x + 1, 0);
+                }
+                if (is_first_entered)
+                {
+                    is_first_entered = false;
+                    player_script.onDamaged(target_pos);
+                    GameManager.instance.OnPlayerDead();
+                }
+            }
+            
+            if(!isReflectable) return;
+            
             // draw first collision point
-            predictionLine.SetPosition(1, predictionHit.point);
-    
+            predictionLine.SetPosition(1, new Vector3(predictionHit.point.x + transform.position.x, predictionHit.point.y + transform.position.y, 0));
+            
             // calculate second ray by Vector2.Reflect
             var inDirection = (predictionHit.point - (Vector2)transform.position).normalized;
             var reflectionDir = Vector2.Reflect(inDirection, predictionHit.normal);
@@ -83,8 +106,7 @@ public class MirrorReflect : MonoBehaviour
                     GameManager.instance.OnPlayerDead();
                 }
             }
-
-        predictionLine.SetPosition(2, predictionHit.point);
+            predictionLine.SetPosition(2, predictionHit.point);
     
             // finally render linerenderer
             predictionLine.enabled = true;
