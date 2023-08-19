cd4c45c6e2b107197a376e75418396ab60e8f3f0
dubidubob <sildredel@naver.com>
2023-08-19 21:02:39 +0900

    이끼 벽 스크립트

--- Assets/MossWall.cs
+++ Assets/MossWall.cs
@@ -4,8 +4,25 @@ using UnityEngine;
 
 public class MossWall : MonoBehaviour
 {
+    public GameObject other;
+
     private void OnCollisionStay2D(Collision2D collision)
     {
-        collision.gameObject.CompareTag("Player")
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("뭐임?");
+            Rigidbody otherRigidbody = other.GetComponent<Rigidbody>();
+            otherRigidbody.constraints &= ~RigidbodyConstraints.FreezePositionY;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("졌다?");
+            Rigidbody otherRigidbody = other.GetComponent<Rigidbody>();
+            otherRigidbody.constraints = RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotation;
+        }
     }
 }
