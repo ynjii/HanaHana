e7794644fba3d50ae0a417cca75a99666be88371
ruruisryu <79790536+ruruisryu@users.noreply.github.com>
2023-08-25 02:46:09 +0900

    서현 성맵 완료

--- Assets/Scripts/Obstacle/SnowWhite_Rope.cs
+++ Assets/Scripts/Obstacle/SnowWhite_Rope.cs
@@ -2,21 +2,15 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-/*
+
 public class SnowWhite_Rope : MonoBehaviour
 {
     [SerializeField] private List<SpriteRenderer> _ropeSprites;
     [SerializeField] private Sprite _burningSprite;
 
-    private void Start()
-    {
-        _burningSprite.
-        StartCoroutine(BurnRope());
-    }
-
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Enemy"))
+        if (other.CompareTag("Player"))
         {
             StartCoroutine(BurnRope());
         }
@@ -24,11 +18,14 @@ public class SnowWhite_Rope : MonoBehaviour
 
     IEnumerator BurnRope()
     {
+        yield return new WaitForSeconds(0.7f);
         for (int i = 0; i < _ropeSprites.Count - 1; i++)
         {
-            yield return new WaitForSeconds(0.3f);
+            yield return new WaitForSeconds(0.12f);
             _ropeSprites[i].gameObject.SetActive(false);
             _ropeSprites[i+1].sprite = _burningSprite;
+            _ropeSprites[i + 1].transform.rotation = Quaternion.Euler(new Vector3(0, 0, -90));
+            _ropeSprites[i + 1].transform.localScale = new Vector3(2, 2, 2);
         }
     }
-}*/
+}
