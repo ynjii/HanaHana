740a878eda365d994661d4d3acb5c548a4b9a717
hyunmin0809 <khm010809@naver.com>
2023-08-24 23:40:58 +0900

    Fix: 패턴 정리

--- Assets/Scripts/Boss/Pattern_2/ThrowObj.cs
+++ Assets/Scripts/Boss/Pattern_2/ThrowObj.cs
@@ -10,33 +10,77 @@ public class ThrowObj : MonoBehaviour
     public float force = 10f;
     private float timeSinceLastSpew = 0f;
 
+    public enum ObjType
+    {
+        typeA,
+        typeB
+    }
+
+    [SerializeField]
+    private ObjType objType;
+
     private void Update()
     {
         timeSinceLastSpew += Time.deltaTime;
 
         if (timeSinceLastSpew >= spewInterval)
         {
-            StartSpewing();
+
+            switch (objType)
+            {
+                case ObjType.typeA:
+                    StartSpewingA();
+                    break;
+
+                case ObjType.typeB:
+                    StartSpewingB();
+                    break;
+            }
         }
     }
 
-    private void StartSpewing()
+    private void StartSpewingA()
     {
         timeSinceLastSpew = 0f;
 
         if (objPrefab != null)
         {
-            GameObject objInstance = Instantiate(objPrefab, transform.position, Quaternion.identity);
-            Rigidbody2D objRigidbody = objInstance.GetComponent<Rigidbody2D>();
-            if (objRigidbody != null)
-            {
-                // 랜덤한 방향과 힘을 생성하여 불을 튀어오르게 만듦
-                Vector3 randomDirection = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0f).normalized;
+            // 시작 방향과 최종 방향 설정
+            Vector3 startDirection = new Vector3(0f, -1f, 0f);
+            Vector3 targetDirection = new Vector3(0f, 1f, 0f);
 
-                objRigidbody.AddForce(randomDirection * force, ForceMode2D.Impulse);
+            // 각도 증가 단계
+            float angleStep = 30f;
+
+            for (float angle = 0f; angle <= 360f; angle += angleStep)
+            {
+                // 각도를 Quaternion으로 변환하여 방향 계산
+                Quaternion rotation = Quaternion.Euler(0f, 0f, angle);
+                Vector3 direction = rotation * startDirection;
+
+                GameObject objInstance = Instantiate(objPrefab, transform.position, Quaternion.identity);
+                Rigidbody2D objRigidbody = objInstance.GetComponent<Rigidbody2D>();
+
+                if (objRigidbody != null)
+                {
+                    objRigidbody.AddForce(direction * force, ForceMode2D.Impulse);
+                }
+
+                Destroy(objInstance, spewDuration);  // 불을 spewDuration 시간 후에 파괴
             }
+        }
+    }
+
+    private void StartSpewingB()
+    {
+        timeSinceLastSpew = 0f;
+
+        if (objPrefab != null)
+        {
+
 
             Destroy(objInstance, spewDuration);  // 불을 spewDuration 시간 후에 파괴
         }
     }
+
 }
