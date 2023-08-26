a2cea791eca87d80375ca13ae4e4c0e3804b2def
ynjii <kimyunji@ewhain.net>
2023-08-26 14:40:24 +0900

    Fix: 스테이지씬 팝업 추가

---
+++ Assets/Scripts/Sound/PopupManager2.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopupManager2 : MonoBehaviour
+{
+    public GameObject popup2; // 팝업창 게임 오브젝트
+
+    private void Start()
+    {
+        // 팝업창 초기화
+        popup2.SetActive(false);
+    }
+
+    // 팝업창 활성화
+    public void ShowPopup()
+    {
+        popup2.SetActive(true);
+    }
+
+    // 팝업창 닫기
+    public void ClosePopup()
+    {
+        popup2.SetActive(false);
+    }
+
+}
