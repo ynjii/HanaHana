e34482b124dcdd2e3f28b7167aba92e367e85083
dubidubob <2076397@ewhain.net>
2023-08-27 23:02:47 +0900

    트램폴린으로 바뀜

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -60,8 +60,6 @@ public class GameManager : MonoBehaviour
         }
 
         player.GetComponent<Player>().ChangeSprites();
-        
-        PlayerPrefs.DeleteAll();
     }
 
     void Update()
