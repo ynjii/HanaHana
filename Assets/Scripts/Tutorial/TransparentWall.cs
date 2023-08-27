f81670db514468bf2a27eb819114971cb88dfe3a
YujinJeon888888 <117812553+YujinJeon888888@users.noreply.github.com>
2023-08-27 15:36:14 +0900

    FIx: 안 쓰는 update문 삭제

--- Assets/Scripts/Tutorial/TransparentWall.cs
+++ Assets/Scripts/Tutorial/TransparentWall.cs
@@ -4,17 +4,12 @@ using UnityEngine;
 
 public class TransparentWall : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
 
     private void Awake()
     {
         if (PlayerPrefs.GetString("TransparentWall") == "False")
         {
-            Debug.Log("투명벽삭제하기");
             this.GetComponent<BoxCollider2D>().enabled = false;
         }
     }
