e0dfea491105a04b248343ab78afe908a459000e
YujinJeon888888 <117812553+YujinJeon888888@users.noreply.github.com>
2023-08-31 11:25:00 +0900

    fix: 모바일버전// 보스패턴4를 뺌.

--- Assets/Scripts/Obstacle/Portal.cs
+++ Assets/Scripts/Obstacle/Portal.cs
@@ -5,24 +5,34 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using static Define;
 using UnityEngine.UI;
+using Unity.VisualScripting;
 
 public class Portal : MonoBehaviour
 {
-    [SerializeField] private Image fadeOutPanel;
-    [SerializeField] private string _nextScene;
+    //[SerializeField] private Image fadeOutPanel;
+    //[SerializeField] private string _nextScene;
+
+    public GameObject clearUI;
+    public AudioSource clap;
     private bool fade_out = false;
     private float timer = 0;
+    private bool clear = false;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (!other.CompareTag("Player")) return;
-        fade_out = true;
-        Transform parent_trans=fadeOutPanel.gameObject.transform.parent;
-        parent_trans.gameObject.SetActive(true);
+        /*        fade_out = true;
+                Transform parent_trans=fadeOutPanel.gameObject.transform.parent;
+                parent_trans.gameObject.SetActive(true);
+        */
+        clearUI.SetActive(true);
+        clap.Play();
+        clear = true;
+        
     }
 
     private void Update()
     {
-        if (fade_out)
+        /*if (fade_out)
         {
             timer += Time.deltaTime;
             fadeOutPanel.color+= new Color(0,0,0,0.1f);
@@ -30,8 +40,24 @@ public class Portal : MonoBehaviour
         if (timer >= 1f)
         {
             SceneManager.LoadScene(_nextScene);
+        }*/
+        if (clear)
+        {
+            timer += Time.deltaTime;
+            
         }
+        if (timer >= 3)
+        {
+            touchTorestart();
+        }
+        
     }
 
-
+    private void touchTorestart()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene("MainScene");
+        }
+    }
 }
