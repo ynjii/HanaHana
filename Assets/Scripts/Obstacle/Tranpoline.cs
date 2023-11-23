b32191da1a3d72257150474128c48f2ee5420710
hyunmin0809 <khm010809@naver.com>
2023-11-23 14:48:36 +0900

    Fix: Tranpoline

--- Assets/Scripts/Obstacle/Tranpoline.cs
+++ Assets/Scripts/Obstacle/Tranpoline.cs
@@ -5,17 +5,18 @@ using UnityEngine;
 public class Tranpoline : MonoBehaviour
 {
 
-    [SerializeField]
-    private GameObject player;
+
 
     [SerializeField]
     private float jumpPower = 500f;
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Rigidbody2D playerRigidbody = player.GetComponent<Rigidbody2D>();
+
         if (collision.gameObject.CompareTag("Player")) //여기서 player 가 내려오는 속도때문에 덜 올라가는게 있는데 여기에 player.velocity = 0f 추가해주면 될듯
         {
+            Rigidbody2D playerRigidbody = collision.gameObject.GetComponent<Rigidbody2D>();
             Vector2 forceDirection = new Vector2(0, 1);
+            playerRigidbody.velocity = Vector2.zero;
             playerRigidbody.AddForce(forceDirection * jumpPower * 1000); //player 위로 올려보내기.
         }
     }
