8793a83bfd674cd8dc278d9e3aa5cd3bd5f00956
YujinJeon888888 <117812553+YujinJeon888888@users.noreply.github.com>
2023-11-26 23:50:33 +0900

    add: 코드정리

--- Assets/Scripts/Util/Util.cs
+++ Assets/Scripts/Util/Util.cs
@@ -6,15 +6,40 @@ using UnityEngine;
 /// </summary>
 public class Util : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+  
+    /// <summary>
+    /// 찍은 꼭짓점대로 움직임. //유진
+    /// 사용예시: SnowBoss4.cs 
+    /// targetPositions: 꼭짓점 리스트
+    /// transform: 움직일 오브젝트의 Transform
+    /// moveSpeed: 움직일 스피드
+    private void MoveToPoints(List<Vector3> targetPositions,  float moveSpeed, Transform transform)
     {
+        int currentTargetIndex = 0;
+        //위치점 찍어놨던 배열에서 하나씩 꺼냄
+        Vector3 targetPosition = targetPositions[currentTargetIndex];
+        // 현재 위치와 목표 위치 사이의 거리 계산
+        float distanceToTarget = Vector3.Distance(transform.position, targetPosition);
+        // 일정 거리 내에 있으면 다음 목표 위치로 변경
+        if (distanceToTarget <= 0.1f) // 예시로 0.1f를 사용, 원하는 값으로 조정 가능
+        {
+            currentTargetIndex = (currentTargetIndex + 1) % targetPositions.Count;
+            targetPosition = targetPositions[currentTargetIndex];
+        }
 
+        // 목표 위치로 이동
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, Time.deltaTime * moveSpeed);
     }
-
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// 애니메이터 바꿔주기
+    /// path: 경로명
+    /// anim: 바꿔줄 오브젝트의 애니메이터
+    /// </summary>
+    public void ChangeAnimator(Animator anim, string path)
     {
+        anim.runtimeAnimatorController =
+            (RuntimeAnimatorController)RuntimeAnimatorController.Instantiate(Resources.Load(path,
+                typeof(RuntimeAnimatorController)));
 
     }
 }
