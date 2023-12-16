1823166aab1d46a0270ced3cb7664a821d283eb6
YujinJeon888888 <yujinjun@ewhain.net>
2023-12-16 15:44:43 +0900

    add: RotateController

--- Assets/Scripts/Obstacle/Rotate_DeltaAxis.cs
+++ Assets/Scripts/Obstacle/Rotate_DeltaAxis.cs
@@ -12,6 +12,7 @@ public class Rotate_DeltaAxis : MonoBehaviour
     [SerializeField] private Collider2D collisionCollider;
 
     /// <summary>
+    /// ex) a_Tree2
     /// 트리거가 감지되면 현재위치에서 point를 더한 위치를 기준으로 rotateDelta만큼 회전
     /// </summary>
     /// <param name="other"></param>
