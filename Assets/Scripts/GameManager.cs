bf83fda99951aca96351d86254922a7d849f7673
YujinJeon888888 <117812553+YujinJeon888888@users.noreply.github.com>
2024-01-13 22:55:26 +0900

    add: nowyoucanswim장애물, crab

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -53,13 +53,13 @@ public class GameManager : MonoBehaviour
         //리스폰 위치로 플레이어 위치를 reset함.
         //보스씬들은 리스폰위치에서 태어나면x
 
-        if (SceneManager.GetActiveScene().name != Define.Scene.SnowBoss4.ToString() && SceneManager.GetActiveScene().name != "SnowBoss1" && SceneManager.GetActiveScene().name != "SnowBoss2" && SceneManager.GetActiveScene().name != "SnowBoss3")
+  /*      if (SceneManager.GetActiveScene().name != Define.Scene.SnowBoss4.ToString() && SceneManager.GetActiveScene().name != "SnowBoss1" && SceneManager.GetActiveScene().name != "SnowBoss2" && SceneManager.GetActiveScene().name != "SnowBoss3")
         {
             if (SaveLoad.GetComponent<SaveLoad>().LoadRespawn("respawn") != Vector3.zero)
             {
                 player.transform.position = SaveLoad.GetComponent<SaveLoad>().LoadRespawn("respawn");
             }
-        }
+        }*/
 
 
         player.GetComponent<Player>().ChangeSprites();
